c104cae5fa96bd318cf59fc1dc4dcda032ff909a
Jan Konarski <37694742+Nexonus@users.noreply.github.com>
2025-05-26 23:22:37 +0200

    Added Block and Creative Tab

---
+++ src/main/java/dev/nexreon/tfcmineralogy/init/BlockInit.java
@@ -0,0 +1,23 @@
+package dev.nexreon.tfcmineralogy.init;
+
+import dev.nexreon.tfcmineralogy.TFCMineralogy;
+import net.minecraftforge.common.Tags.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.block.SoundType;
+
+
+public class BlockInit {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TFCMineralogy.MODID);
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", 
+    () -> new Block(BlockBehaviour.Properties.of()
+    .mapColor(MapColor.COLOR_GRAY)
+    .strength(6.0F)
+    .sound(SoundType.STONE)
+    .requiresCorrectToolForDrops()));
+}
---
+++ src/main/java/dev/nexreon/tfcmineralogy/init/ItemInit.java
@@ -0,0 +1,26 @@
+package dev.nexreon.tfcmineralogy.init;
+
+import java.rmi.registry.Registry;
+import dev.nexreon.tfcmineralogy.TFCMineralogy;
+import net.minecraftforge.common.Tags.Items;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static dev.nexreon.tfcmineralogy.init.CreativeTabInit.addToTab;
+
+public class ItemInit {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TFCMineralogy.MODID);
+    public static final RegistryObject<Item> EXAMPLE_ITEM = addToTab(ITEMS.register("example_item",
+        () -> new Item(new Item.Properties()
+        .stacksTo(64)
+        .rarity(Rarity.COMMON))));
+    
+    public static final RegistryObject<BlockItem> EXAMPLE_BLOCK_ITEM = addToTab(ITEMS.register("example_block",
+        () -> new BlockItem(BlockInit.EXAMPLE_BLOCK.get(), new Item.Properties()
+        .rarity(Rarity.COMMON))));
+}
