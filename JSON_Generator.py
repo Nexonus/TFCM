6185554e83afe9e2ea874fcb0b4e16f408e798ec
Jan Konarski <37694742+Nexonus@users.noreply.github.com>
2025-05-27 09:23:53 +0200

    First Example Ore Gen

--- JSON_Generator.py
+++ JSON_Generator.py
@@ -6,27 +6,43 @@ RESOURCES_DIR = os.path.join(ROOT_DIR, 'src/main/resources')
 
 rm = ResourceManager(domain='tfcmineralogy', resource_dir=RESOURCES_DIR, indent=2, ensure_ascii=False)
 rm_tfc = ResourceManager(domain='tfc', resource_dir=RESOURCES_DIR, indent=2, ensure_ascii=False)
+rm_forge = ResourceManager(domain='forge', resource_dir=RESOURCES_DIR, indent=2, ensure_ascii=False)
 os.makedirs(RESOURCES_DIR, exist_ok=True)
 
-rm.placed_feature('vein/example_block', 'tfcmineralogy:vein/example_block') # This is the vein we're looking for in configured feature
-
-### CREATE VIVIANITE FEATURE
-rm.configured_feature('vein/example_block', 'tfc:cluster_vein', {'rarity': 110, 'density': 0.85, 'min_y': -45, 'max_y': -12, 'size': 8,'random_name': 'example_block',
-'blocks':[
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':'tfcmineralogy:example_block'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:example_block'}]}
-    #{'replace':['tfc:rock/raw/basalt'],'with':[{'block':'tfcmineralogy:ore/vivianite/marble'}]}
+### CREATE AN ORE GEN FEATURE
+mineral_name="example_block"
+rm.placed_feature(f'vein/{mineral_name}', f'tfcmineralogy:vein/{mineral_name}') # This is the vein we're looking for in configured feature
+rm.configured_feature(f'vein/{mineral_name}', 'tfc:cluster_vein', {'rarity': 60, 'density': 0.2, 'min_y': -120, 'max_y': 120, 'size': 16,'random_name': f'{mineral_name}',
+'blocks':[  # Comment the ones we don't want to use for the generation.
+    {'replace':['tfc:rock/raw/diorite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/gabbro'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/shale'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/claystone'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/limestone'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/conglomerate'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/dolomite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/chert'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/chalk'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/basalt'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/andesite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/dacite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/quartzite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/slate'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/phyllite'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/schist'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/gneiss'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]},
+    {'replace':['tfc:rock/raw/marble'],'with':[{'block':f'tfcmineralogy:{mineral_name}'}]}
 ]
 })
 rm.flush()
 
-rm_tfc.tag('veins','worldgen/placed_feature/in_biome','tfcmineralogy:vein/example_block')
+rm_tfc.tag('veins','worldgen/placed_feature/in_biome',f'tfcmineralogy:vein/{mineral_name}')
+rm_tfc.tag('prospectable','blocks',f'tfcmineralogy:{mineral_name}')
 rm_tfc.flush()
+
+rm_forge.tag(f'{mineral_name}','blocks/ores', f'tfcmineralogy:{mineral_name}')
+rm_forge.tag('ores','blocks',f'#forge:ores/{mineral_name}')
+rm_forge.flush()
+
+
