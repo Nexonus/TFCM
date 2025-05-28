9cafdd1a70fe3c167fbd3ca262db071564f4e895
Jan Konarski <37694742+Nexonus@users.noreply.github.com>
2025-05-28 10:08:07 +0200

    Script updated
    We'll see if it doesnt break everything

--- Vein_Generator.py
+++ Vein_Generator.py
@@ -20,6 +20,7 @@ forge_ores = set()
 stone_mineral_vein_dict = set() # wip
 
 ### MODEL : NO WEIGHTS, SINGULAR MINERAL (HALITE ETC)
+# Comment here to hide
 """
 for mineral_name in mineral_dict:
     vein_feature_names.add(f'vein/{mineral_name}')
@@ -55,13 +56,17 @@ for mineral_name in mineral_dict:
         rm_forge.tag(f'{mineral_name}','blocks/ores', f'tfcmineralogy:ore/{stone}_{mineral_name}')
         tfcm_prospectables.add(f'tfcmineralogy:ore/{stone}_{mineral_name}')
 """
+#Stop comment here
+
 ### MODEL : INCLUDING WEIGHTS (POOR, NORMAL, RICH)
 mineral_name = 'smithsonite'
-qualities = {'poor','normal','rich'}
+qualities = ['poor','normal','rich']
+resource_list = list()
+for q in qualities:
+    mineral_quality = q+mineral_name
+    resource_list.append(mineral_quality)   # Populate list here
 
-weight_poor = {55, 30, 20}
-weight_normal = {30, 40, 35}
-weight_rich = {15, 30, 45}
+weight = [55, 30, 15]
 
 for q in qualities:
     mineral_quality = q+mineral_name
@@ -69,33 +74,32 @@ for q in qualities:
     tfcm_veins.add(f'tfcmineralogy:vein/{mineral_quality}')
     forge_ores.add(f'#forge:ores/{mineral_quality}')
     rm.placed_feature(f'vein/{mineral_quality}', f'tfcmineralogy:vein/{mineral_quality}') # This is the vein we're looking for in configured feature
-    rm.configured_feature(f'vein/{mineral_quality}', 'tfc:cluster_vein', {'rarity': 60, 'density': 0.2, 'min_y': -120, 'max_y': 120, 'size': 16,'random_name': f'{mineral_quality}',
-'blocks':[  # Comment the ones we don't want to use for the specific generation.
-    # Include Weights
-    {'replace':['tfc:rock/raw/diorite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/diorite_{mineral_name}'},{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/diorite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/gabbro'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/gabbro_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/shale'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/shale_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/claystone'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/claystone_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/limestone'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/limestone_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/conglomerate'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/conglomerate_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/dolomite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/dolomite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/chert'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/chert_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/chalk'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/chalk_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/rhyolite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/basalt'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/basalt_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/andesite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/andesite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/dacite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/dacite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/quartzite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/quartzite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/slate'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/slate_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/phyllite'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/phyllite_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/schist'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/schist_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/gneiss'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/gneiss_{mineral_name}'}]},
-    {'replace':['tfc:rock/raw/marble'],'with':[{'weight':f'{weight_poor}','block':f'tfcmineralogy:ore/marble_{mineral_name}'}]}
-]
-})
-for stone in stone_dict:
-    rm_forge.tag(f'{mineral_name}','blocks/ores', f'tfcmineralogy:ore/{stone}_{mineral_name}')
-    tfcm_prospectables.add(f'tfcmineralogy:ore/{stone}_{mineral_name}')
+    rm.configured_feature(f'vein/{mineral_quality}', 'tfc:cluster_vein', {'rarity': 60, 'density': 0.2, 'min_y': 45, 'max_y': 120, 'size': 25,'random_name': f'{mineral_quality}',
+    'blocks':
+    [   # With Weights Applied
+    {'replace':['tfc:rock/raw/diorite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/diorite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/diorite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/diorite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/gabbro'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/gabbro_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/gabbro_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/gabbro_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/shale'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/shale_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/shale_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/shale_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/claystone'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/claystone_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/claystone_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/claystone_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/limestone'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/limestone_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/limestone_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/limestone_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/conglomerate'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/conglomerate_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/conglomerate_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/conglomerate_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/dolomite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/dolomite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/dolomite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/dolomite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/chert'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/chert_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/chert_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/chert_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/chalk'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/chalk_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/chalk_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/chalk_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/rhyolite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/rhyolite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/rhyolite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/rhyolite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/basalt'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/basalt_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/basalt_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/basalt_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/andesite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/andesite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/andesite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/andesite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/dacite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/dacite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/dacite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/dacite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/quartzite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/quartzite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/quartzite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/quartzite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/slate'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/slate_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/slate_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/slate_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/phyllite'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/phyllite_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/phyllite_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/phyllite_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/schist'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/schist_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/schist_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/schist_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/gneiss'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/gneiss_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/gneiss_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/gneiss_{resource_list[2]}'}]},
+    {'replace':['tfc:rock/raw/marble'],'with':[{'weight':f'{weight[0]}','block':f'tfcmineralogy:ore/marble_{resource_list[0]}'},{'weight':f'{weight[1]}','block':f'tfcmineralogy:ore/marble_{resource_list[1]}'},{'weight':f'{weight[2]}','block':f'tfcmineralogy:ore/marble_{resource_list[2]}'}]}
+    ]})
+    for stone in stone_dict:
+        rm_forge.tag(f'{mineral_quality}','blocks/ores', f'tfcmineralogy:ore/{stone}_{mineral_quality}')
+        tfcm_prospectables.add(f'tfcmineralogy:ore/{stone}_{mineral_quality}')
 
 
 
@@ -108,17 +112,4 @@ rm_tfc.tag('prospectable','blocks',*tfcm_prospectables)
 rm_forge.tag('ores','blocks',*forge_ores)
 rm_tfc.flush()
 rm_forge.flush()
-
-"""
-rm_tfc.tag('veins','worldgen/placed_feature/in_biome',f'tfcmineralogy:vein/{mineral_name}')
-rm_tfc.tag('prospectable','blocks',f'tfcmineralogy:{mineral_name}')
-rm_forge.tag('ores','blocks',f'#forge:ores/{mineral_name}')
-rm_tfc.flush()
-rm_forge.flush()
-"""
-
-
-#rm_forge.tag(f'{mineral_name}','blocks/ores', f'tfcmineralogy:{mineral_name}')
-#rm_forge.tag('ores','blocks',f'#forge:ores/{mineral_name}')
-#rm_forge.flush()
 
